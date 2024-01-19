fb4bba83cfcc8ebbc727d8dc2210c5d84385133d
Edson José dos Santos <edsonj82@outlook.com>
2024-01-19 20:05:12 -0300

    Installing & Configuring Faker library

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -1,11 +1,16 @@
 /// <reference types="cypress" />
 
+import { faker } from '@faker-js/faker';
+// or, if desiring a different locale
+
+faker.person.jobType()
+
 describe('tasks', ()=>{
     it('should register a new task', ()=>{
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type('Ler um livro de Node.js')
+            .type(faker.person.jobType() + ' - Step by Step')
 
             //button[contains(text(),'Create')]
             cy.contains('button', 'Create').click()
