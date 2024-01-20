e05166960d33f9d4b90fb61f95ad332a0c9f2b85
Edson José dos Santos <edsonj82@outlook.com>
2024-01-20 12:11:36 -0300

    Consuming 'remove task by name' api

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -2,6 +2,17 @@
 
 describe('tasks', ()=>{
     it('should register a new task', ()=>{
+        cy.request({
+            url:'http://localhost:3333/helper/tasks',
+            method:'DELETE',
+            body:{
+                name:'Ler um livro de nodejs'
+            }
+        }).then(response=>{
+            expect(response.status).to.eq(204)
+        })
+
+
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
