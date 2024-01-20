a9b59d40e42bdbb0ea54f3787fa16a27e2fb38b3
Edson José dos Santos <edsonj82@outlook.com>
2024-01-19 21:35:25 -0300

    Remove faker-js

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -1,16 +1,11 @@
 /// <reference types="cypress" />
 
-import { faker } from '@faker-js/faker';
-// or, if desiring a different locale
-
-faker.person.jobType()
-
 describe('tasks', ()=>{
     it('should register a new task', ()=>{
         cy.visit('http://localhost:8080')
 
         cy.get('input[placeholder="Add a new Task"]')
-            .type(faker.person.jobType() + ' - Step by Step')
+            .type('Ler um livro de nodejs')
 
             //button[contains(text(),'Create')]
             cy.contains('button', 'Create').click()
