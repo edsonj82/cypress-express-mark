3e7432b28926d815c2f0a7d14e47c8b7f403139b
Edson José dos Santos <edsonj82@outlook.com>
2024-01-21 20:50:01 -0300

    Applying independents tests (wip)

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -28,4 +28,20 @@ describe('tasks', ()=>{
         cy.contains('main div p', 'Ler um livro de nodejs')
             .should('be.visible')
     })
+
+    it('should not allow duplated task',()=>{
+        
+        cy.visit('http://localhost:8080')
+
+        cy.get('input[placeholder="Add a new Task"]')
+            .type('Ler um livro de nodejs')
+
+        //button[contains(text(),'Create')]
+        cy.contains('button', 'Create').click()
+
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text','Task already exists!')
+
+    })
 })
