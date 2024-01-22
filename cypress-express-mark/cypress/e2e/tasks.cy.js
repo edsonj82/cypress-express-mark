320c57c3b97f1cc27016ebc4d589223b767c6be4
Edson José dos Santos <edsonj82@outlook.com>
2024-01-22 18:26:28 -0300

    Customizing task Commands

--- cypress-express-mark/cypress/e2e/tasks.cy.js
+++ cypress-express-mark/cypress/e2e/tasks.cy.js
@@ -1,67 +1,57 @@
 /// <reference types="cypress" />
 
-describe('tasks', ()=>{
-    it('should register a new task', ()=>{
+describe('tasks', () => {
+    it('should register a new task', () => {
         const taskName = 'Ler um livro de nodejs'
 
-        cy.request({
-            url:'http://localhost:3333/helper/tasks',
-            method:'DELETE',
-            body:{ name: taskName }
-        }).then(response =>{
-            expect(response.status).to.eq(204)
-        })
-
-
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(taskName)
-
-        //button[contains(text(),'Create')]
-        cy.contains('button', 'Create').click()
-
+        cy.removeTaskByName(taskName)
+        cy.createTask(taskName)
         cy.contains('main div p', taskName)
             .should('be.visible')
     })
 
-    it('should not allow duplated task',()=>{
-        const task ={
+    it('should not allow duplated task', () => {
+        const task = {
             name: 'Estudar javascript',
             is_done: false
-        }    
+        }
 
-        cy.request({
-            url:'http://localhost:3333/helper/tasks',
-            method:'DELETE',
-            body:{
-                name: task.name
-            }
-        }).then(response =>{
-            expect(response.status).to.eq(204)
-        })
-
+        cy.removeTaskByName(task.name)
         //Dado que eu tenho uma tarefa duplicada
-        cy.request({
-            url:'http://localhost:3333/tasks',
-            method:'POST',
-            body: task
-        }).then(response =>{
-            expect(response.status).to.eq(201)
-        })
-
+        cy.postTask(task)
         //Quando faço o cadastro dessa tarefa
-        cy.visit('http://localhost:8080')
-
-        cy.get('input[placeholder="Add a new Task"]')
-            .type(task.name)
-
-        cy.contains('button', 'Create').click()//button[contains(text(),'Create')]
-
+        cy.createTask(task.name)
         //Então vejo a mensagem de duplicidade
         cy.get('.swal2-html-container')
             .should('be.visible')
-            .should('have.text','Task already exists!')
+            .should('have.text', 'Task already exists!')
+    })
+})
 
+Cypress.Commands.add('createTask', (taskName) => {
+    cy.visit('http://localhost:8080')
+
+    cy.get('input[placeholder="Add a new Task"]').type(taskName)
+
+    cy.contains('button', 'Create').click()    //button[contains(text(),'Create')]
+})
+
+Cypress.Commands.add('removeTaskByName', (taskName) => {
+    cy.request({
+        url: 'http://localhost:3333/helper/tasks',
+        method: 'DELETE',
+        body: { name: taskName }
+    }).then(response => {
+        expect(response.status).to.eq(204)
+    })
+})
+
+Cypress.Commands.add('postTask', (task) => {
+    cy.request({
+        url: 'http://localhost:3333/tasks',
+        method: 'POST',
+        body: task
+    }).then(response => {
+        expect(response.status).to.eq(201)
     })
 })
